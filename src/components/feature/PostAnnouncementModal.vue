06dc1277c126874bbce5cc6ac6ae741dc54f5142
John Arias <john@thinkbitsolutions.com>
2023-03-31 19:47:03 +0800

    Fix: Modal
    N/A

--- src/components/feature/PostAnnouncementModal.vue
+++ src/components/feature/PostAnnouncementModal.vue
@@ -1,7 +1,12 @@
 <template>
-  <input id="post-announcement-modal" type="checkbox" class="modal-toggle" />
+  <input
+    id="post-announcement-modal"
+    class="modal-toggle block"
+    type="checkbox"
+    v-model="postAnnouncementModal"
+  />
 
-  <label for="post-announcement-modal" class="modal cursor-pointer">
+  <div class="modal cursor-pointer" @click="postAnnouncementModal = false">
     <div class="modal-box">
       <h3 class="font-bold text-lg text-center">CREATE A NEW ANNOUNCEMENT</h3>
       <div class="mt-4 py-4">
@@ -28,18 +33,28 @@
         <div class="mb-5 flex justify-between"></div>
       </div>
       <div class="modal-action flex justify-between">
-        <label for="post-announcement-modal" class="modal cursor-pointer">
-          <a
-            href="#"
-            class="btn bg-transparent text-black border-none hover:bg-slate-200"
-          >
-            Close
-          </a>
-        </label>
-        <a href="#" class="btn bg-primary-500 border-none hover:bg-primary-700">
+        <a
+          href="#"
+          class="btn bg-transparent text-black border-none hover:bg-slate-200"
+          @click="postAnnouncementModal = false"
+        >
+          Close
+        </a>
+        <a
+          href="#"
+          class="btn bg-primary-500 border-none hover:bg-primary-700"
+          @click="postAnnouncementModal = false"
+        >
           Create
         </a>
       </div>
     </div>
-  </label>
+  </div>
 </template>
+
+<script setup>
+// Base Imports
+import { ref } from 'vue';
+
+const postAnnouncementModal = ref(false);
+</script>
